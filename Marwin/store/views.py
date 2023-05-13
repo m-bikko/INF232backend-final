ab3ab38835ef235c4d7b6a66df1f38b4990ec329
Amir <210103034@stu.sdu.edu.kz>
2023-05-13 16:29:32 +0600

    Работает Логин, создал сис админ, добавил некоторые функций

--- Marwin/store/views.py
+++ Marwin/store/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
+def store(request):
+    context = {}
+    return render(request, 'store/Main.html')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, f'{username},You are logged in.')
+            return redirect("store")
+        else:
+            messages.info(request, 'Wrong username or password')
+            return redirect('login')
+    return render(request, 'store/log.html')
+
+
+def register_user(request):
+    context = {}
+    return render(request, 'store/sign.html')
