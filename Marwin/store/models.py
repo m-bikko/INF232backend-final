1d95a25164054157f6177769c27560fba511c576
Amir <210103034@stu.sdu.edu.kz>
2023-05-13 17:45:23 +0600

    Models Customer, Product, Order, OrderItem

--- Marwin/store/models.py
+++ Marwin/store/models.py
@@ -1,6 +1,41 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django.contrib.auth.models import User
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    prices = models.FloatField(null=True, blank=True)
+    descript = models.CharField(max_length=200, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.set_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.set_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.prices * self.quantity
+        return total
